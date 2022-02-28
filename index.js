0ef342be6e94d873776e1749e956175fddfe6524
Maria Rey <maria.x.rey.-nd@disney.com>
2022-02-28 20:32:46 -0300

    observaciones

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const obtenerUsuarios = () =>{
     fetch(`https://rickandmortyapi.com/api/character`)
     .then(res => res.json())
     .then(data => {
+        // no dejes console log en una entrega
         console.log(data)
         tarjeta(data.results)
         clickTarjeta(data.results)
     })
 }    
 
+// es preferible que estas variables esten definidas arriba de todo
 //botones prev y next
 const contenedor = document.querySelector(".contenedor")
 const prev = document.querySelector("#prev")
@@ -16,11 +18,14 @@ const next = document.querySelector("#next")
 let paginaActual = 1
 
 next.onclick = () => {
+    // preferible escribir paginaActual++
     paginaActual = paginaActual + 1
 
     fetch(`https://rickandmortyapi.com/api/character?${paginaActual}`)
     .then((res) => res.json())
     .then((data) => {
+        // no hay ninguna funcion que se llame contenedor, esto provoca que tu codigo falle. 
+        // quiza querias decir tarjeta(data.results)
         contenedor(data.results)
     })
 }
@@ -29,11 +34,14 @@ next.onclick = () => {
 
 
 prev.onclick = () => {
+    // preferible escribir paginaActual--
     paginaActual = paginaActual - 1
 
     fetch(`https://rickandmortyapi.com/api/character?page=${paginaActual}`)
     .then((res) => res.json())
     .then((data) => {
+                // no hay ninguna funcion que se llame contenedor, esto provoca que tu codigo falle. 
+        // quiza querias decir tarjeta(data.results)
         contenedor(data.results)
     })
 }
@@ -59,7 +67,11 @@ const clickTarjeta = ()=>{
     const contenedorDetalles = document.querySelector(".contenedor-detalles")
 
     for (let i = 0; i < imgBotones.length; i++) {
+        // se mas prolija, respeta los espacios: es como en un texto, dejás espacios despues de un punto, 
+        // una coma, antes de un parentesis. Hace lo mismo en JSON
+        // imgBotones[i].onclick= () => {
         imgBotones[i].onclick=()=>{
+            // no dejes codigo comentado en una entrega
             // console.log(imgBotones[i].id);
             contenedor.style.display = "none"
             contenedorDetalles.style.display = "flex"
@@ -92,6 +104,8 @@ const mostrarTarjetaDetalles = (idDelBoton) => {
     fetch(`https://rickandmortyapi.com/api/character/${idDelBoton}`)
     .then((res) =>  res.json())
     .then((data) => {
+        // ojo con el tabulado aca, las funciones deberian llamarse en donde estoy
+        // escribiendo este comentario, no a la misma altura del then
     detalles(data)
     volverListadoUsuario()
     })
---
+++ observaciones.md
@@ -0,0 +1,21 @@
+Caro, 
+
+Felicitaciones por la entrega de tu trabajo! Sé que ni el tiempo ni la compu te jugaron a favor pero me alegra que hayas entregado una web funcional. 
+
+Como siempre, te dejo varios comentarios a lo largo de tus archivos. A nivel contenidos, creo que tu TP demuestra la comprensión de los temas vistos, y no tengo muchos comentarios sobre la lógica o tu javascript en particular. 
+
+De tu web en sí estoy satisfecha, pero me gustaría que siguieras trabajando en ella, ya que podrías aprender mucho sobre lo ya hecho. en particular, sería importantisimo que hagas el buscador, ya que tiene un grado de complejidad mayor y es algo que con toda seguridad vas a tener que hacer en los primeros meses en algun trabajo. 
+
+Los botones de paginado no funcionan, pero creo que es porque estas llamando a la funcion incorrecta. Te lo dejo comentado. 
+
+Tambien estaria bueno que le pongas un poco mas de onda al diseño si queres publicar esta web en tu portfolio. 
+
+Donde veo más problemas es en los temas que ya espero manejes con seguridad. Los dejo comentados, pero insisto aquí en algunos puntos:
+
+- La desprolijidad de tu código es una constante a lo largo de todos tus trabajos y es algo que te va a restar muchísimos puntos en una entrevista técnica. Ni en CSS, ni en JS hay un buen tabulado. En CSS se hace realmente muy dificil entender las cosas porque hay cero onda puesta en tabular. Dejás saltos de línea al azar, no tabulás correctamente, no dejás los espacios necesarios entre las cosas.
+
+- Si bien lo ideal sería que aprendas a ser mas prolija por vos misma, sé que el tiempo muchas veces no es un aliado. Te super animo a que te bajes la extensión `prettier` para VSCode y te familiarices con su uso. Revisá siempre antes de una entrega todos tus archivos y corré la extensión para cada uno de ellos. 
+
+- No dejes codigo comentado ni console log. 
+
+Nota final: 7
